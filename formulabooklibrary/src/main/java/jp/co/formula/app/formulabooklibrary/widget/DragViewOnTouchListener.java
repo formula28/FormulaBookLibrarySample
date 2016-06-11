7d5f37f90b41382b244c3c899a7f646fdf8a03d2
formula <formula22_1@yahoo.co.jp>
2016-06-11 12:54:48 +0900

    [FormulaBookLibrary] ドラッグ可能化用OnTouchListener追加

---
+++ formulabooklibrary/src/main/java/jp/co/formula/app/formulabooklibrary/widget/DragViewOnTouchListener.java
@@ -0,0 +1,67 @@
+package jp.co.formula.app.formulabooklibrary.widget;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Viewをドラッグして位置を移動できるようにするためのOnTouchListener実装.
+ * Created by @formula on 2016/06/11.
+ */
+abstract public class DragViewOnTouchListener implements View.OnTouchListener {
+    /** ドラッグ中フラグ. */
+    private boolean mIsDragStarted = false;
+    /** ドラッグするView. */
+    private View mDragView;
+    /** 前回タッチ位置(X). */
+    private float mOldX = 0f;
+    /** 前回タッチ位置(Y). */
+    private float mOldY = 0f;
+
+    /**
+     * コンストラクタ.
+     * @param aDragView ドラッグ対象のView.
+     */
+    public DragViewOnTouchListener(View aDragView) {
+        mDragView = aDragView;
+    }
+    /**
+     * ドラッグ開始判定(タッチダウン時にドラッグを開始するかの判定方法を実装すること).
+     * @return true:ドラッグ開始する, false:ドラッグ開始しない.
+     */
+    abstract public boolean isDragStart();
+
+    /**
+     * ドラッグ終了判定(タッチアップ時にドラッグを開始するかの判定方法を実装すること).
+     * @return true:ドラッグ終了する, false:ドラッグ終了しない.
+     */
+    abstract public boolean isDragEnd();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+        case MotionEvent.ACTION_DOWN:
+            mIsDragStarted = isDragStart();
+            mOldX = motionEvent.getRawX();
+            mOldY = motionEvent.getRawY();
+            break;
+        case MotionEvent.ACTION_UP:
+            mIsDragStarted = !isDragEnd();
+            break;
+        case MotionEvent.ACTION_MOVE:
+            if (mDragView != null && mIsDragStarted) {
+                int left = mDragView.getLeft() + (int)(motionEvent.getRawX() - mOldX);
+                int top = mDragView.getTop() + (int)(motionEvent.getRawY() - mOldY);
+                mDragView.layout(left, top, left + mDragView.getWidth(), top + mDragView.getHeight());
+                mOldX = motionEvent.getRawX();
+                mOldY = motionEvent.getRawY();
+            }
+            break;
+        default:
+            break;
+        }
+        return true;
+    }
+}
---
+++ samples/dragviewsample/src/main/java/jp/co/formula/app/dragviewsample/MainActivity.java
@@ -0,0 +1,29 @@
+package jp.co.formula.app.dragviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import jp.co.formula.app.formulabooklibrary.widget.DragViewOnTouchListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.helloworld);
+        text.setOnTouchListener(new DragViewOnTouchListener(text){
+
+            @Override
+            public boolean isDragStart() {
+                return true;
+            }
+
+            @Override
+            public boolean isDragEnd() {
+                return true;
+            }
+        });
+    }
+}
